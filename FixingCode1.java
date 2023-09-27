a9b711b3c9e5766d4cd2e11a089573eb3279e44c
BONK24B <52805361+BONK24B@users.noreply.github.com>
2023-09-27 15:52:51 +0300

    Add files via upload

---
+++ FixingCode1.java
@@ -0,0 +1,16 @@
+package HW1;
+
+
+public class FixingCode1 {
+    public static void main(String[] args){
+        try {
+            int d = 0;
+            int[] intArray = new int[9];
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }     
+    }
+}
